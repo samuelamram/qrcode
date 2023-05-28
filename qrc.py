18c8a5fea622098486e45482dddf45cd3be8e80e
samuelamram <32494990+samuelamram@users.noreply.github.com>
2023-05-29 00:10:03 +0200

    Update algorithm for 5th anniversary

--- qrc.py
+++ qrc.py
@@ -1,11 +1,19 @@
-import qrcode
-from datetime import datetime as dt
 import os
+from datetime import datetime as dt
 import click
+import qrcode
 from config import friends # from config.py (not commited in Git)
 
 LETTRES = "ABCDEFGHIJ"
-DICO = { LETTRES[i]:str(i) for i in range(10) }
+# DICO = { LETTRES[i]:str(i) for i in range(10) }
+
+# Codes pour le 5ème anniversaire
+LETTRES5 = "BCDEFGHJKMNPQRST"
+HEXA = "0123456789abcdef"
+# DICO5 = { HEXA[i]:LETTRES5[i] for i in range(16)}
+
+DICOS = {'4': { LETTRES[i]:str(i) for i in range(10) },
+         '5': { HEXA[i]:LETTRES5[i] for i in range(16)}}
 
 # Le fichier config.py doit contenir un dictionnaire friends défini par exemple comme suit :
 
@@ -27,26 +35,58 @@ def char2time(code, dico):
 
     return timestamp
 
-def time2char(qr_time, dico):
+def time2char(qr_time):
     """
     Calcule le code alphanumérique à partir d'un datetime.
     
     Parameters:
         qr_time (dt): le datetime à convertir
-        dico (str): le dictionnaitre pour décoder le code (correspondance entre lettre et chiffre)
 
     Returns:
         str: Code alphanumérique
     """
     qr_timestamp = qr_time.timestamp() * 1000
     qr_timestamp_str = str(qr_timestamp)[:13]
-    print(f'qr_timestamp_str={qr_timestamp_str}')
+    click.echo(f'qr_timestamp_str={qr_timestamp_str}')
     return ''.join([LETTRES[int(i)] for i in qr_timestamp_str])
 
-
-def create_qrcode(friend, dir, dico):
+def generate_data(qr_time, friend, dico, version):
     """
-    Crée un QR Code compatible.
+    Calcule le data à mettre dans le QR Code
+
+    Parameters:
+        qr_time (dt): le timestamp au format datetime
+        friend (str): le code ami
+        dico (str): le dictionnaitre pour décoder le code (correspondance entre lettre et chiffre)
+        version (int): l'algorithme à utiliser
+
+    Returns:
+        str: Code alphanumérique    
+    """
+
+    # 4th anniversary
+    if version == 4:
+        click.echo('----- Version 4 ----')
+        data = ','.join(['4', friend['id'], time2char(qr_time)])
+    # 5th anniversary
+    elif version == 5:
+        click.echo('----- Version 5 ----')
+        # calculer le timestamp en ms
+        qr_timestamp = int(qr_time.timestamp() * 1000)
+        # convertir en hexadecimal
+        qr_timestamp_hex = hex(qr_timestamp)[2:]
+        # remplacer les lettres selon le dico5
+        qr_timestamp_code = ''.join([dico[i] for i in qr_timestamp_hex])
+        data = '4,'+friend['id']+qr_timestamp_code
+    else:
+        data = ''
+
+    return data
+
+
+def create_qrcode(version, friend, output_dir, dico):
+    """
+    Crée un QR Code compatible pour le 4ème anniversaire.
     
     Parameters:
         friend (dict): un dictionnaire décrivant l'ami : {'id':'abcd', 'name':'mon_nom'}
@@ -54,24 +94,41 @@ def create_qrcode(friend, dir, dico):
 
     """
     now = dt.now()
-    data = ','.join(['4', friend['id'], time2char(now, DICO)])
-
+ #   data = ','.join(['4', friend['id'], time2char(now, dico)])
+    data =  generate_data(now, friend, dico, version)
+    click.echo(f'data={data}')
     img =  qrcode.make(data)
 
-    filename = os.path.join(dir, friend['name'] + now.strftime('%Y%m%d%H%M%S') + '.png')
+    filename = os.path.join(output_dir, friend['name'] + now.strftime('%Y%m%d%H%M%S') + '.png')
 
     img.save(filename)
 
     return now
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+@cli.command()
 @click.option('--dir', type=click.Path(exists=True, dir_okay=True), default='img/')
-def create_qrcodes(dir):
+@click.option('--version', default=4)
+def c(dir, version):
+    """
+    Creates QR Codes
+    """
     click.echo(f'dir={dir}')
     for f in friends:
-        now = create_qrcode(f, dir, DICO)
-        print(f'QR code created with timestamp {now}')
+        now = create_qrcode(version, f, dir, DICOS[str(version)])
+        click.echo(f'QR code created with timestamp {now}')
 
+@cli.command()
+@click.option('--ts', help='encoded timestamp')
+def d(ts):
+    """
+    Decodes timestamp
+    """
+    click.echo(f'timestamp={ts}')
+    click.echo(f'Decoded timestamp={char2time(ts, DICOS["4"])}')
 
 if __name__ == '__main__':
-    create_qrcodes()
+    cli()
